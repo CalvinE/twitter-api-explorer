d924dbeed40b5fc7859e2505558c280046b3b74f
Calvin Echols <calvin.echols@gmail.com>
2020-09-04 17:48:35 -0400

    fixed type

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import configparser
 
-def readConfig(config_file = "sample_config.ino"):
+def readConfig(config_file = "sample_config.ini"):
     config = configparser.ConfigParser()
     config.read(config_file)
     return config
